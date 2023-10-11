f2a349614f20f781137d5e7a3b213373195aaf7d
Anna <anna.chymshyt@gmail.com>
2023-10-12 02:38:04 +0300

    Add entity

---
+++ src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
@@ -0,0 +1,28 @@
+package com.example.app.SpringBootWebClients.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "phone")
+    private String phone;
+
+}
